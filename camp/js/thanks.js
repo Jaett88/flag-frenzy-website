8210db3fd118fb0256f94a04191fb165903eb002
Jaett88 <missjaet01@gmail.com>
2025-09-23 19:18:01 +0100

    Delete thanks.js

--- camp/js/thanks.js
+++
@@ -1,9 +0,0 @@
-// Insert camper name into thank-you message
-document.addEventListener("DOMContentLoaded", () => {
-  const camperName = localStorage.getItem("camperName");
-  if (camperName) {
-    const greeting = document.getElementById("thank-you-greeting");
-    greeting.textContent = `Thank you for registering ${camperName}!`;
-    localStorage.removeItem("camperName"); // Clear after use
-  }
-});
